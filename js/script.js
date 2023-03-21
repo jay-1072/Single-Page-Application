48b25def9886a130eb9ca03eb9a908db84cb9fb9
Jay Koshti <jay.koshti@simformsolutions.com>
2023-03-21 18:31:07 +0530

    added functions for redirecting

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function callHome() {
+    location.href = "#home";
+}
+
+function callAboutUs() {
+    location.href = "#about";
+}
+
+function callImageSlider() {
+    location.href = "#image";
+}
+
+function callServices() {
+    location.href = "#service";
+}
